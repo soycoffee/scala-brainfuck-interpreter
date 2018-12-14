37928d70883b230cd56b82dbd0b74a4ca771147e
yishii <yishii@altpaper.net>
2018-12-14 16:23:40 +0900

    test rot13

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,14 @@
-trait Main {
+object Main {
 
-  def input: InputState[_]
-  def output: String => Unit
+  def main(args: Array[String]): Unit = main(args, InputState.StdIn, print)
 
-  lazy val loader = Loader()
-  lazy val parser = Parser()
-  lazy val runner = Runner(input)
-
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String], input: InputState[_], output: String => Unit): Unit = {
     assert(args.length >= 1)
+    val loader = Loader()
+    val parser = Parser()
+    val runner = Runner(input)
     val result = (loader andThen parser andThen (_.flatMap(runner)))(args(0))
     output(new String(result.get.output.toArray))
   }
-
-}
-
-object Main extends Main {
-
-  override def input: InputState[_] = InputState.StdIn
-  override def output: String => Unit = print
 
 }
--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -13,19 +13,21 @@ class MainSpec extends FunSpec {
 
   }
 
-  private def buildMain(inputValues: Seq[Byte], myOutput: MyOutput): Main =
-    new Main {
-      override val input: InputState[_] = InputState.SeqIn(inputValues)
-      override val output: String => Unit = myOutput
-    }
-
   describe("apply") {
 
     it("hello_world") {
       val myOutput = new MyOutput
-      val main = buildMain(Seq(), myOutput)
-      main.main(Array("hello_world.brainfuck"))
+      Main.main(Array("hello_world.brainfuck"), InputState.NilIn, myOutput)
       assert(myOutput.last.get == "Hello World!\n")
+    }
+
+    it("rot13") {
+      val allLatinAlphabets = (0x41 to 0x5a) ++ (0x61 to 0x7a)
+      val eof = -0x01
+      val inputValues = (allLatinAlphabets :+ eof).map(_.toByte)
+      val myOutput = new MyOutput
+      Main.main(Array("rot13.brainfuck"), InputState.SeqIn(inputValues), myOutput)
+      assert(myOutput.last.get == "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm")
     }
 
   }
